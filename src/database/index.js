7e524c71bb50c478f8f5a842e02d61195466fef0
agnaldo26 <agnaldosilva9704@gmail.com>
2021-09-26 18:32:03 -0300

    trocado as imagens salvas por links

--- src/database/index.js
+++ src/database/index.js
@@ -1,28 +1,23 @@
-import bigImg1 from '../assets/big-img1.jpg';
-import bigImg2 from '../assets/big-img2.jpg';
-import bigImg3 from '../assets/big-img3.jpg';
-import bigImg4 from '../assets/big-img4.jpg';
-
 const Images = [
     {
         id: 1,
         title: 'a Corebiz atua em toda jornada digital do usuário.',
-        url: bigImg1,
+        url: 'https://images.unsplash.com/photo-1542744173-8e7e53415bb0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
     },
     {
         id: 2,
         title: 'temos clientes fixos ou projetos em 29 países.',
-        url: bigImg2,
+        url: 'https://images.unsplash.com/photo-1544906243-a69767cc000b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80',
     },
     {
         id: 3,
         title: 'temos mais de 340 colaboradores pelo mundo,',
-        url: bigImg3,
+        url: 'https://images.unsplash.com/photo-1504384308090-c894fdcc538d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
     },
     {
         id: 4,
         title: 'e mais de 100 clientes ativos em diversos países.',
-        url: bigImg4,
+        url: 'https://images.unsplash.com/photo-1557804506-669a67965ba0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=774&q=80',
     }
 ]
 
