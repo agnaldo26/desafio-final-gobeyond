0cbeaf8b5d04d1df23ad0c345be042a2aff3fb4c
agnaldo26 <agnaldosilva9704@gmail.com>
2021-09-25 21:27:46 -0300

    feito a integração com o database de imagens e títulos

--- src/components/DinamicContent/index.js
+++ src/components/DinamicContent/index.js
@@ -1,9 +1,6 @@
 import React, { useState } from 'react';
-import './style.css'
-import bigImg1 from '../../assets/big-img1.jpg';
-import bigImg2 from '../../assets/big-img2.jpg';
-import bigImg3 from '../../assets/big-img3.jpg';
-import bigImg4 from '../../assets/big-img4.jpg';
+import './style.css';
+import Images from '../../database'
 import arrowSeeMore from '../../assets/arrow-see-more.svg';
 
 function DinamicContent() {
@@ -14,7 +11,7 @@ function DinamicContent() {
 
     const Img1 = ()=>{
         return(
-            <img src={bigImg1} alt='Imagem pequena' key='img1' onClick={() =>{
+            <img src={Images[0].url} alt='Imagem pequena' key='img1' onClick={() =>{
                 setSelectImg1(true)
                 setSelectImg2(false)
                 setSelectImg3(false)
@@ -25,7 +22,7 @@ function DinamicContent() {
 
     const Img2 = ()=>{
         return(
-            <img src={bigImg2} alt='Imagem pequena' key='img2' onClick={() =>{
+            <img src={Images[1].url} alt='Imagem pequena' key='img2' onClick={() =>{
                 setSelectImg2(true)
                 setSelectImg1(false)
                 setSelectImg3(false)
@@ -36,7 +33,7 @@ function DinamicContent() {
 
     const Img3 = ()=>{
         return(
-            <img src={bigImg3} alt='Imagem pequena' key='img3' onClick={() =>{
+            <img src={Images[2].url} alt='Imagem pequena' key='img3' onClick={() =>{
                 setSelectImg3(true)
                 setSelectImg1(false)
                 setSelectImg2(false)
@@ -47,7 +44,7 @@ function DinamicContent() {
 
     const Img4 = ()=>{
         return(
-            <img src={bigImg4} alt='Imagem pequena' key='img4' onClick={() =>{
+            <img src={Images[3].url} alt='Imagem pequena' key='img4' onClick={() =>{
                 setSelectImg4(true)
                 setSelectImg1(false)
                 setSelectImg2(false)
@@ -58,25 +55,25 @@ function DinamicContent() {
 
     let BigImg = () =>{
         if(selectImg1){
-            return <img src={bigImg1} alt='Imagem grande' className='big-img' />
+            return <img src={Images[0].url} alt='Imagem grande' className='big-img' />
         } else if(selectImg2){
-            return <img src={bigImg2} alt='Imagem grande' className='big-img' />
+            return <img src={Images[1].url} alt='Imagem grande' className='big-img' />
         } else if(selectImg3){
-            return <img src={bigImg3} alt='Imagem grande' className='big-img' />
+            return <img src={Images[2].url} alt='Imagem grande' className='big-img' />
         } else if(selectImg4){
-            return <img src={bigImg4} alt='Imagem grande' className='big-img' />
+            return <img src={Images[3].url} alt='Imagem grande' className='big-img' />
         }
     }
 
     let Text = ()=>{
         if(selectImg1){
-            return <h2 className='title'>a Corebiz atua em toda jornada digital do usuário.</h2>
+            return <h2 className='title'>{Images[0].title}</h2>
         } else if(selectImg2){
-            return <h2 className='title'>Desde a configuração de plataformas, implementações e integrações,</h2>
+            return <h2 className='title'>{Images[1].title}</h2>
         } else if(selectImg3){
-            return <h2 className='title'>até a entrega de uma excelente experiência de consumo.</h2>
+            return <h2 className='title'>{Images[2].title}</h2>
         } else if(selectImg4){
-            return <h2 className='title'>trabalhamos para uma melhor performance digital.</h2>
+            return <h2 className='title'>{Images[3].title}</h2>
         }
     }
 
@@ -85,18 +82,15 @@ function DinamicContent() {
             <div className='div-title'>
                 <Text />
             </div>
-
             <div className='div-see-more'>
                 <a href='https://www.corebiz.ag/pt/' target='_blank' rel="noreferrer" className='link-see-more'>
                     <span className='text-see-more'>veja mais</span>
                     <img src={arrowSeeMore} alt='flexa veja mais' className='arrow-see-more' />
                 </a>
             </div>
-
             <div className='container-big-img'>
                 <BigImg/>
             </div>
-
             <div className='container-little-imgs'>
                 <Img1 />
                 <Img2 />
@@ -107,4 +101,4 @@ function DinamicContent() {
     )
 }
 
-export default DinamicContent
+export default DinamicContent;
--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,7 @@
-import bigImg1 from '../assets/big-img1.png'
-import bigImg2 from '../assets/big-img2.png'
-import bigImg3 from '../assets/big-img3.png'
-import bigImg4 from '../assets/big-img4.png'
+import bigImg1 from '../assets/big-img1.jpg'
+import bigImg2 from '../assets/big-img2.jpg'
+import bigImg3 from '../assets/big-img3.jpg'
+import bigImg4 from '../assets/big-img4.jpg'
 
 const Images = [
     {
@@ -11,17 +11,17 @@ const Images = [
     },
     {
         id: 2,
-        title: 'Desde a configuração de plataformas, implementações e integrações, até a entrega de uma excelente experiência de consumo.',
+        title: 'temos clientes fixos ou projetos em 29 países',
         url: bigImg2,
     },
     {
         id: 3,
-        title: 'Juntamente com toda a equipe de marketing trabalhando para uma melhor performance digital,',
+        title: 'temos mais de 340 colaboradores pelo mundo',
         url: bigImg3,
     },
     {
         id: 4,
-        title: 'o que qualifica ainda mais a entrega da jornada do usuário.',
+        title: 'e mais de 100 clientes ativos em diversos países',
         url: bigImg4,
     }
 ]
